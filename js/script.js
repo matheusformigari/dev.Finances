a891f30e9f369cdf6f3e7b8df749caaf433b97e1
Matheus Formigari <86173896+matheusformigari@users.noreply.github.com>
2021-10-13 23:34:43 -0300

    toques finais da parte visual, inicio da implementação das funcionalidades

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.querySelector('div.modal-overlay').classList.add('active')
+}
+
+function closeModal() {
+    document.querySelector('div.modal-overlay').classList.remove('active')
+}
